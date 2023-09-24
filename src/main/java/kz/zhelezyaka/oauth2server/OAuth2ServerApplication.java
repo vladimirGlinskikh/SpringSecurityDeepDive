a391798b95d220016778e214d40bfd864a33216c
Vladimir Glinskikh <vladimir.v.glinskikh@gmail.com>
2023-09-24 22:09:10 +0300

    Start project

---
+++ src/main/java/kz/zhelezyaka/oauth2server/OAuth2ServerApplication.java
@@ -0,0 +1,13 @@
+package kz.zhelezyaka.oauth2server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OAuth2ServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OAuth2ServerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/kz/zhelezyaka/oauth2server/OAuth2ServerApplicationTests.java
@@ -0,0 +1,13 @@
+package kz.zhelezyaka.oauth2server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OAuth2ServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
